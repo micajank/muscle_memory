3b63446735e7163050c674e2a3ff6dff6cd6de38
micajank <micajank@umich.edu>
2020-07-17 14:07:54 -0700

    Add 5 variables

---
+++ app.js
@@ -0,0 +1,21 @@
+// 5 variables of different data types
+// string
+let aString = "Hello i am a string";
+console.log(aString);
+// number
+let aNumber = 13;
+console.log(aNumber);
+// array
+const anArray = ["boom", "boom", "boom"];
+console.log(anArray)
+// boolean
+let aBoolean = false;
+console.log(aBoolean);
+// object (4 different key-value pairs) [string], [number], [array], [boolean]
+const anObject = {
+    name: "Mac",
+    age: 25,
+    favoriteFoods: ["cheese", "wine", "beans"],
+    readyToRumble: true
+}
+console.log(anObject);
